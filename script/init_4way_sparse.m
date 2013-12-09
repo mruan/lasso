a5782f01719051d38f1d8403ed66fa076e649900
Ming Ruan <mhr@cmu.edu>
2013-12-08 20:14:58 -0500

    Adding the files. TODO: condense the matrix block

---
+++ script/init_4way_sparse.m
@@ -0,0 +1,58 @@
+function [L, C, D, N] = init_4way_sparse(img)
+[H, W, Depth] = size(img);
+if(Depth==3) % if rgb image -> grayscale image
+  img = rgb2gray(img);
+end
+
+%% Convert to BW binary image
+bw_img = img < 128;
+
+beta_idx_lin = find(bw_img);  % we are interested in these pixels...
+N = numel(beta_idx_lin);      % dimension of the problem
+[beta_idx_u, beta_idx_v] = ind2sub([H, W], beta_idx_lin);
+
+L = zeros(3*N);
+D = zeros(3*N);
+C = zeros([3*N, 3*N, 4]);
+
+% helper function sub2idx
+sub2idx =@(y, x) find(beta_idx_lin == sub2ind([H, W], y, x));
+
+% Find the normalizing scale
+sc = 1/max([W, H]); 
+for i=1:N
+  u = beta_idx_u(i);
+  v = beta_idx_v(i);
+  
+  k = (i-1)*3+1:i*3;
+  L(k, k) = [u*sc v*sc 1]'*[u*sc v*sc 1];
+  D(k, k) = diag([1 1 0]);
+  
+  % Find all neighbors of pixel indexed at i:
+  if(u>1 && bw_img(u-1,v))
+    t = sub2idx(u-1, v); r = (t-1)*3+1:t*3;
+    C(k, k, 1) = diag([1,1,1]);
+    C(k, r, 1) = diag([-1,-1,-1]);
+  end
+  
+  if(v>1 && bw_img(u,v-1))
+    t = sub2idx(u, v-1); r = (t-1)*3+1:t*3;
+    C(k, k, 2) = 1;
+    C(k, r, 2) = diag([-1,-1,-1]);
+  end
+  
+  if(u<H && bw_img(u+1,v))
+    t = sub2idx(u+1, v); r = (t-1)*3+1:t*3;
+    C(k, k, 3) = diag([1,1,1]);
+    C(k, r, 3) = diag([-1,-1,-1]);
+  end
+  
+  if(v<W && bw_img(u,v+1))
+    t = sub2idx(u, v+1); r = (t-1)*3+1:t*3;
+    C(k, k, 4) = diag([1,1,1]);
+    C(k, r, 4) = diag([-1,-1,-1]);
+  end
+end
+
+end
+
---
+++ script/test_csp_sparse.m
@@ -0,0 +1,57 @@
+clear;
+dbstop if error;
+
+%% Load Image:
+img = imread('nov21/horline.png');
+
+[L, C, D, N] = init_4way_sparse(img);
+
+%d  = zeros([3*N, 1]); d(3:3:end) = -1; % d'*x <= 0 <=> c_i >= 0 
+d  = zeros([N, 3*N]);
+for i=1:N
+   d(i, 3*i) = -1; 
+end
+
+xi = 10.0;  % noramally called lambda
+mu = 1.0;
+lb = zeros([14*N,1]); ub = lb;
+lb(1:12*N) = -xi; lb(13*N+1:end) = [];
+ub(1:12*N) =  xi; ub(12*N+1:end) = [];
+
+%% The problem is:
+%% min (0.5*z'(A'/Q*A)z+(b'/Q*A+l)z  
+%% where z = [u_{1-4} v w];
+%% x_op = -Q\(Az+b)
+
+%%  y
+y = zeros([3*N, 1]);
+y(1:3:end) = -1; y(3:3:end) =0.5;
+
+%% these are independent of y
+Q = L+mu*D; % Q = sparse(Q); 
+Q_inv = eye(size(Q))/Q;
+l = zeros([1, 14*N]); l(end-N:end)=1;
+Aeq = []; beq = zeros(4*2*N,1);
+for i=1:4*N
+   Aeq = blkdiag(Aeq, [1 -1 0; 0 1 -1]); 
+end
+Aeq = [Aeq zeros(2*4*N, 2*N)];
+
+%% Make Y matrix
+Y = zeros([3*N, N]);
+for i=1:N
+   Y(3*(i-1)+1: 3*(i-1)+2, i) = y(3*(i-1)+1: 3*(i-1)+2);
+end
+b = -mu*D*y;
+
+A = [C(:,:,1)', C(:,:,2)', C(:,:,3)', C(:,:,4)', d', Y];
+A = sparse(A);
+
+%% let H=(A'/Q*A), f=(b'/Q*A+l)
+H = A'*Q_inv*A;
+f = b'*Q_inv*A+l;
+
+% x = quadprog(H,f,A,b,Aeq,beq,lb,ub)
+opts = optimoptions('quadprog','Algorithm','active-set','Display','off');
+[z_op, fval] = quadprog(H, f, [], [], Aeq, beq, lb, ub, [], opts);
+x_op = -Q_inv*(A*z_op+b);
